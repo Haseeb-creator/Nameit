c8d1a724d8f07ba9c6eddc792256faf54470366b
Haseeb-creator <mdhaseeb73@gmail.com>
2021-04-19 13:55:34 +0530

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ResultContainer from "../ResultsContainer/ResultContainer";
+import SearchBox from "../SearchBox/SearchBox";
+import Header from "./../Header/Header";
+import "./App.css";
+
+const name = require("@rstacruz/startup-name-generator");
+
+class App extends React.Component {
+  state = {
+    headerText: "Name it!",
+    headerExpanded: true,
+    suggestedNames: [],
+  };
+
+  handleInputChange = (inputText) => {
+    this.setState({
+      headerExpanded: !inputText,
+      suggestedNames: inputText ? name(inputText) : [],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header
+          headerExpanded={this.state.headerExpanded}
+          headTitle={this.state.headerText}
+        />
+        <SearchBox onInputChange={this.handleInputChange} />
+        <ResultContainer suggestedNames={this.state.suggestedNames} />
+      </div>
+    );
+  }
+}
+
+export default App;
